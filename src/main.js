b1eaf0969517ed9cb9535f41dd81d639278b418b
KedProgrammer <shiriux1@gmail.com>
2017-09-12 01:03:34 -0500

    beta 3.0

--- src/main.js
+++ src/main.js
@@ -8,17 +8,17 @@ import borrar from './delete.png';
 
 
 class Main extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     
      this.start = this.start.bind(this);
     this.state = {
       textos: [
       {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: "Estudiar", proyect:"Make it real" ,done: true},
-      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar" ,tittle: "", proyect: "", done: true}
+      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar" ,tittle: "Hacer Tareas", proyect: "Colegio", done: true}
       ],
-      tittle: "",
-      proyect: ""
+      error: [{new:false},{form:false}],
+      newtask:false
     }
   }
 
@@ -29,13 +29,47 @@ class Main extends Component {
         <div className='container'>
          {this.state.textos.map((text, index) =>   
 	    				<div className="main">
-	    					{ text.done ? this.task(text,index) : this.form(index)}
-	    				</div>		
+	    					{ text.done ? this.task(text,index) : this.form(text,index)}
+	    				</div>
         )}
-         <Button bsStyle="info" onClick={this.addTask.bind(this)} >Nueva tarea</Button>
+        {this.new()}
+        {this.state.newtask ? null : <Button bsStyle="info" onClick={this.handle_form.bind(this)} >Nueva tarea</Button> }
         </div>
     )
   }
+
+new(){
+  if (this.state.newtask){
+    return (
+      <div className="main">
+        <div className="child">
+          <form onSubmit={this.prueba.bind(this)} >
+              <div >
+                <div >
+                 <div >
+                  <span className="proyect">Title</span>
+                 </div>
+                 </div>
+                 <div>
+                   <FormControl name="title-input" value={this.state.tittle} className={this.state.error[0].new ? "stop input" : "input"} />
+                 </div>
+                  <div>
+                  <span className="proyect">Proyect</span>
+                 </div>
+                 <div >
+                   <FormControl name="proyect-input"  value={this.state.proyect} className={this.state.error[0].new ? "stop input" : "input"} />
+                 </div>
+                 <div className="group-buttons" >
+                    <Button type='submit' className="form" id="create"><span>Crear</span></Button>
+                    <Button onClick={this.handle_form.bind(this)} className="form" id="cancel"><span>Cancel</span></Button> 
+                 </div>
+              </div>
+          </form>
+         </div>
+      </div>
+    )
+  }
+}
 
 task(text,index){
 	return(
@@ -46,7 +80,8 @@ task(text,index){
         <div className="center">{text.texto}</div>
         <div className="image-container">
           <img src={image} alt="my image" onClick={() => this.toggleDone(index)} />
-          <img src={borrar} alt="my image" onClick={() => this.delete(index)} />
+          <img src={borrar} al
+           onClick={() => this.delete(index)} />
         </div>
      </div>
 
@@ -55,12 +90,10 @@ task(text,index){
 		)
 }
 
-form(index){
-
-	
+form(task,index){
 	return(
 	<div className="child">
-		<form>
+		<form onSubmit={(e) => this.setUp(index,e)} >
 				<div >
 					<div >
 					 <div >
@@ -68,17 +101,17 @@ form(index){
 					 </div>
 					 </div>
 					 <div>
-					 	 <FormControl onChange={this.setTitle.bind(this)} className="input" />
+					 	 <FormControl  value={task.tittle} className={this.state.error[1].form ? "stop input" : "input"}  name="title-input-form" />
 					 </div>
 					  <div>
 					 	<span className="proyect">Proyect</span>
 					 </div>
 					 <div >
-					 	 <FormControl onChange={this.setProyect.bind(this)} className="input" />
+					 	 <FormControl  value={task.proyect} className={this.state.error[1].form ? "stop input" : "input"}  name="proyect-input-form"   />
 					 </div>
 					 <div className="group-buttons" >
-					    <Button onClick={() => this.setUp(index)}  className="form" id="create"><span>Create</span></Button>
-					    <Button className="form" id="cancel"><span>Canacel</span></Button> 
+					    <Button type='submit' className="form" id="create"><span> Editar</span></Button>
+					    <Button onClick={() => this.toggleDone(index)} className="form" id="cancel"><span>Canacel</span></Button> 
 					 </div>
 				</div>
 			
@@ -106,6 +139,9 @@ form(index){
     
     ,1000)  
   }
+
+
+
   addId(id,index){
     this.setState({
       textos: this.state.textos.map((text,i) => {
@@ -118,16 +154,15 @@ form(index){
     })
   }
 
+
+
   stop(id,index){
     this.toggle(index)
     clearInterval(id);
   }
 
-  addTask(){
-    this.setState({
-      textos: this.state.textos.concat({texto: millisecondsToHuman(0), id:0, milisegundos: 1000,state: "Comenzar", tittle:""})
-    })
-  }
+
+
 
   toggle(index){
       this.setState({
@@ -144,6 +179,9 @@ form(index){
     })
   }
 
+
+
+
   toggleDone(index){
   	this.setState({
   		  textos: this.state.textos.map((text,i) => {
@@ -156,45 +194,91 @@ form(index){
   }
 
 
-  setTitle(e){
-  	this.setState({
-  		 tittle: e.target.value
-  	})
+
+
+
+  setUp(index,e){
+    e.preventDefault();
+  const titlef = e.target['title-input-form'].value;
+  const proyectf = e.target['proyect-input-form'].value;
+
+    if (this.validate(titlef,proyectf)){
+  		this.setState({
+    		  textos: this.state.textos.map((text,i) => {
+    		  	if(index === i){
+    		  		text.tittle = titlef
+    		  		text.proyect = proyectf 
+    		  	}
+    		  	return text
+          }),
+          error: this.state.error.map((text,i) => {
+          if (i === 1){
+             text.form = false
+          }
+          return text
+          })
+    	});
+        this.toggleDone(index)
+    }else{
+      this.setState({
+        error: this.state.error.map((text,i) => {
+          if (i === 1){
+             text.form = true
+          }
+          return text
+          })
+      });
+    }
   }
 
 
-  setProyect(e){
-  	this.setState({
-  		 proyect: e.target.value
-  	})
+
+
+
+
+prueba(event){
+  event.preventDefault()
+	const title = event.target['title-input'].value;
+const proyect = event.target['proyect-input'].value;
+
+if (this.validate(title,proyect)){
+this.setState({
+    textos: this.state.textos.concat({texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: title, proyect: proyect ,done: true}),
+    error: this.state.error.map((text,i) => {
+          if (i === 0){
+             text.new = false
+          }
+          return text
+          })
+  });
+  this.handle_form()
+  }else {
+    this.setState({
+    error: this.state.error.map((text,i) => {
+          if (i === 0){
+             text.new = true
+          }
+          return text
+          })
+  });
   }
-
-
-  setUp(index){
-  	this.toggleDone(index)
-		this.setState({
-  		  textos: this.state.textos.map((text,i) => {
-  		  	if(index === i){
-  		  		text.tittle = this.state.tittle 
-  		  		text.proyect = this.state.proyect 
-  		  	}
-  		  	return text
-        })
-  	})
-
-  }
-
-prueba(index){
-		this.setState({
-  		  textos: this.state.textos.map((text,i) => {
-  		  	if(index === i){
-  		  		text.done = true;
-  		  	}
-  		  	return text
-        })
-  	})
-
 }
+
+validate(title,proyect){
+  if (!title || !proyect){
+  return false
+}else{
+  return true
+  }
+}
+
+handle_form(){
+this.setState({
+  newtask: this.state.newtask ? false : true 
+});
+}
+
+
 
 delete(index){
   var tasks = this.state.textos;
@@ -202,6 +286,9 @@ delete(index){
         textos: tasks.slice(0,index).concat(tasks.slice(index + 1))
     })
 }
+
+
+
 
 }
 
