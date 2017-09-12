9f600eea65cafcbe56dfd224e434d9770058a88e
daniel tobon <shiriux1@gmail.com>
2017-09-12 16:58:12 -0500

    beta 9.0

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react';
 import millisecondsToHuman from './chronometer'
-import {Button , FormControl, ButtonGroup } from 'react-bootstrap';
+import {Button , FormControl } from 'react-bootstrap';
 import './App.css';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import  image from './edit.png';
 import borrar from './delete.png';
 
 
 class Main extends Component {
-  constructor(props){
-    super(props);
-    
+  constructor(){
+    super();  
      this.start = this.start.bind(this);
     this.state = {
       textos: [
-      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: "Estudiar", proyect:"Make it real" ,done: true},
-      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar" ,tittle: "Hacer Tareas", proyect: "Colegio", done: true}
+      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: "Estudiar", proyect:"Make it real" ,done: true,error: false,},
+      {texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar" ,tittle: "Hacer Tareas", proyect: "Colegio", done: true,error:false}
       ],
-      error: [{new:false},{form:false}],
-      newtask:false
+      error: false,
+      newtask:false,
+      
     }
   }
 
-
+ /* Renderiza el array de tareas, en cada tarea  muestra el formulario de edicion o la vista del cronometro*/
   render() {
     return (
-     
         <div className='container'>
          {this.state.textos.map((text, index) =>   
-	    				<div className="main">
+	    				<div className="main">                                           
 	    					{ text.done ? this.task(text,index) : this.form(text,index)}
 	    				</div>
         )}
@@ -38,6 +37,9 @@ class Main extends Component {
     )
   }
 
+
+
+/* retorna el formulario para una nueva tarea */
 new(){
   if (this.state.newtask){
     return (
@@ -51,13 +53,13 @@ new(){
                  </div>
                  </div>
                  <div>
-                   <FormControl name="title-input" value={this.state.tittle} className={this.state.error[0].new ? "stop input" : "input"} />
+                   <FormControl name="title-input" value={this.state.tittle} className={this.state.error ? "stop input" : "input"} />
                  </div>
                   <div>
                   <span className="proyect">Proyect</span>
                  </div>
                  <div >
-                   <FormControl name="proyect-input"  value={this.state.proyect} className={this.state.error[0].new ? "stop input" : "input"} />
+                   <FormControl name="proyect-input"  value={this.state.proyect} className={this.state.error ? "stop input" : "input"} />
                  </div>
                  <div className="group-buttons" >
                     <Button type='submit' className="form" id="create"><span>Crear</span></Button>
@@ -71,6 +73,11 @@ new(){
   }
 }
 
+
+
+
+/* retorna la vista principal de tareas */
+
 task(text,index){
 	return(
 		<div className="chronomter">
@@ -79,17 +86,23 @@ task(text,index){
     		<span id="proyect">{text.proyect}</span>
         <div className="center">{text.texto}</div>
         <div className="image-container">
-          <img src={image} alt="my image" onClick={() => this.toggleDone(index)} />
-          <img src={borrar} al
-           onClick={() => this.delete(index)} />
+          <img src={image}  onClick={() => this.toggleDone(index)} />
+          <img src={borrar} 
+           onClick={() => this.delete(text.id,index)} />
         </div>
      </div>
 
-  	<Button id="main-button" className={text.state === "Stop" ? "stop" : "start"}bsStyle="info" onClick={text.state === "Stop" ? () => this.stop(text.id,index) : () => this.start(index)} >{text.state}</Button>
+  	<Button id="main-button" className={text.state === "Stop" ? "stop" : "start"}bsStyle="info" onClick={text.state === "Stop" ? () => this.stop(text.id,index) : () => this.start(text,index)} >{text.state}</Button>
     </div>
 		)
 }
 
+
+
+
+
+
+/* retorna el formulario de edicion de tareas */
 form(task,index){
 	return(
 	<div className="child">
@@ -101,17 +114,17 @@ form(task,index){
 					 </div>
 					 </div>
 					 <div>
-					 	 <FormControl  value={task.tittle} className={this.state.error[1].form ? "stop input" : "input"}  name="title-input-form" />
+					 	 <FormControl onChange={(e) => this.setTitle(e,index)}  value={task.aux1} className={task.error ? "stop input" : "input"}  name="title-input-form" />
 					 </div>
 					  <div>
 					 	<span className="proyect">Proyect</span>
 					 </div>
 					 <div >
-					 	 <FormControl  value={task.proyect} className={this.state.error[1].form ? "stop input" : "input"}  name="proyect-input-form"   />
+					 	 <FormControl onChange={(e) => this.setProyect(e,index)}  value={task.aux2} className={task.error ? "stop input" : "input"}  name="proyect-input-form"   />
 					 </div>
 					 <div className="group-buttons" >
 					    <Button type='submit' className="form" id="create"><span> Editar</span></Button>
-					    <Button onClick={() => this.toggleDone(index)} className="form" id="cancel"><span>Canacel</span></Button> 
+					    <Button onClick={() => this.toggleDone(index)} className="form" id="cancel"><span>Cancelar</span></Button> 
 					 </div>
 				</div>
 			
@@ -122,40 +135,32 @@ form(task,index){
 }
 
 
-   start(index){
+
+
+
+/* Comienza el cronometro */
+   start(task,index){
     this.toggle(index)
    var id = setInterval(() => {
-    this.addId(id,index);
     this.setState({
       textos: this.state.textos.map((text,i) => {
-        if (index === i){
+        if (task.tittle === text.tittle){                           /* No comparo con el indixe porque las tareas se pueden borrar */
         text.texto = millisecondsToHuman(text.milisegundos)
         text.milisegundos = text.milisegundos + 1000
+        text.id = id
         }
         return text
       })
     })
     }
-    
     ,1000)  
   }
 
 
 
-  addId(id,index){
-    this.setState({
-      textos: this.state.textos.map((text,i) => {
-        if (index === i){
-          text.id = id
-        }
-        return text
-        })
-
-    })
-  }
 
 
-
+/* detiene cronometro */
   stop(id,index){
     this.toggle(index)
     clearInterval(id);
@@ -164,6 +169,10 @@ form(task,index){
 
 
 
+
+
+
+/* cambia el estado del array  comenzar, stop o renaudar */
   toggle(index){
       this.setState({
       textos: this.state.textos.map((text,i) => {
@@ -182,11 +191,17 @@ form(task,index){
 
 
 
+
+
+/* maneja la variable "done" de cada tarea que a su vez controla si se muestra la vista principal o el formulario de edicion */
   toggleDone(index){
   	this.setState({
   		  textos: this.state.textos.map((text,i) => {
   		  	if(index === i){
   		  		text.done ?  text.done = false : text.done = true 
+            text.error = false
+            text.aux1 =  text.tittle
+            text.aux2 = text.proyect
   		  	}
   		  	return text
         })
@@ -197,6 +212,10 @@ form(task,index){
 
 
 
+
+
+
+/* Controla y valida el formulario de edicion*/
   setUp(index,e){
     e.preventDefault();
   const titlef = e.target['title-input-form'].value;
@@ -207,26 +226,23 @@ form(task,index){
     		  textos: this.state.textos.map((text,i) => {
     		  	if(index === i){
     		  		text.tittle = titlef
-    		  		text.proyect = proyectf 
+    		  		text.proyect = proyectf
+              text.error = false
+              text.aux1= titlef
+              text.aux2= proyectf
     		  	}
     		  	return text
-          }),
-          error: this.state.error.map((text,i) => {
-          if (i === 1){
-             text.form = false
-          }
-          return text
           })
     	});
         this.toggleDone(index)
     }else{
       this.setState({
-        error: this.state.error.map((text,i) => {
-          if (i === 1){
-             text.form = true
-          }
-          return text
-          })
+      textos: this.state.textos.map((text,i) => {
+            if(index === i){
+              text.error = true
+            }
+            return text
+          })   
       });
     }
   }
@@ -236,42 +252,50 @@ form(task,index){
 
 
 
-prueba(event){
-  event.preventDefault()
-	const title = event.target['title-input'].value;
-const proyect = event.target['proyect-input'].value;
 
-if (this.validate(title,proyect)){
-this.setState({
-    textos: this.state.textos.concat({texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: title, proyect: proyect ,done: true}),
-    error: this.state.error.map((text,i) => {
-          if (i === 0){
-             text.new = false
-          }
-          return text
-          })
-  });
-  this.handle_form()
-  }else {
+
+/* controla y valida el formulario que hace nuevas tareas */
+prueba(event){
+    event.preventDefault()
+  	const title = event.target['title-input'].value;
+    const proyect = event.target['proyect-input'].value;
+
+  if (this.validate(title,proyect)){
     this.setState({
-    error: this.state.error.map((text,i) => {
-          if (i === 0){
-             text.new = true
-          }
-          return text
-          })
-  });
+      textos: this.state.textos.concat({texto: millisecondsToHuman(0), id:0, milisegundos: 1000, state: "Comenzar", tittle: title, proyect: proyect ,done: true}),
+      error: false
+    });
+    this.handle_form()
+    }else {
+      this.setState({
+      error: true
+    });
   }
 }
 
+
+
+
+
+
+
+
+/* Validacion de formularios */
 validate(title,proyect){
   if (!title || !proyect){
   return false
-}else{
-  return true
+  }else{
+    return true
   }
 }
 
+
+
+
+
+
+
+/* maneja la variable que controla si el formulario para crear una tarea debe aparecer o no */
 handle_form(){
 this.setState({
   newtask: this.state.newtask ? false : true 
@@ -280,7 +304,13 @@ this.setState({
 
 
 
-delete(index){
+
+
+
+
+/* borra una tarea y cancela el set inteval */
+delete(id,index){
+  clearInterval(id)
   var tasks = this.state.textos;
       this.setState({
         textos: tasks.slice(0,index).concat(tasks.slice(index + 1))
@@ -288,6 +318,36 @@ delete(index){
 }
 
 
+
+
+
+setTitle(e,index){
+this.setState({
+  textos: this.state.textos.map((text,i) => {
+            if(index === i){
+              text.aux1 = e.target.value
+            }
+            return text
+          })
+      });
+
+}
+
+
+
+
+
+
+setProyect(e,index){
+  this.setState({
+  textos: this.state.textos.map((text,i) => {
+            if(index === i){
+              text.aux2 = e.target.value
+            }
+            return text
+          })
+      });
+}
 
 
 }
